ee0475ed455d657e55428331ec0359f561e6bff5
PaulMarisOUMary <paul.marisoumary@gmail.com>
2025-09-18 18:23:33 +0200

    update as soon as possible

--- polling.py
+++ polling.py
@@ -16,23 +16,22 @@ class StorageUpdater():
             for key in keys_to_remove:
                 cp_storage.pop(key)
             self.storage.update(cp_storage)
-
+    
+    def update_single(self, key: str, value: Any) -> None:
+        with self.storage_lock:
+            self.storage[key] = value
 
 def background_fetch(obd: Connection, storage: Dict[str, Any], storage_lock: Lock, polling_event: Event, polling_error: Event) -> None:
     updater = StorageUpdater(storage, storage_lock)
     stop = polling_event.is_set
 
     while not stop():
-        updates = dict()
-
         for command_name in tuple(storage.keys()):
             try:
                 response = obd.query(commands[command_name])
-                updates[command_name] = response.value
+                updater.update_single(command_name, response.value)
             except KeyError:
-                updates[command_name] = f"Error: {command_name} not found"
+                updater.update_single(command_name, f"Error: {command_name} not found")
             except Exception as e:
                 polling_error.set()
-                updates[command_name] = f"CRITError: {str(e)}"
-
-        updater.update_storage(updates)
+                updater.update_single(command_name, f"CRITError: {str(e)}")
